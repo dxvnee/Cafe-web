36301471df2ed75dccfa6e0700aa70541b19b731
Eigiya Daramuli Kale <egiadaramuli81@gmail.com>
2024-07-08 16:47:50 +0700

    controller update:

--- controller/controllers.admin.js
+++ controller/controllers.admin.js
@@ -26,6 +26,44 @@ const login = async (req, res) => {
     }
 }
 
+const createAdmin = async (req, res) => {
+    try {
+        const { name, password } = req.body;  
+        const admin = await Admin.findOne({
+            name: name
+        })
+
+        if (admin != null){
+            res.send(200).json({ message: "Admin already exists" });
+        }
+
+        const newUser = new Admin({
+            name: name,
+            password: password            
+        })
+
+        await newUser.save();
+        res.send(200).json({ newUser })
+
+    } catch (error) {
+        res.send(200).json({message : error.message});
+    }
+}
+
+const getAllAdmin = async (req, res) => {
+    try {
+        const allAdmin = await Admin.find();
+        
+        res.senda(200).json(allAdmin);
+
+    } catch (error) {
+        res.send(200).json({ message: error.message})
+    }
+}
+
+
 export {
-    login
+    login,
+    createAdmin,
+    getAllAdmin
 }
--- routes/route.admin.js
+++ routes/route.admin.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { login } from '../controller/controllers.admin.js';
+import { createAdmin, getAllAdmin, login } from '../controller/controllers.admin.js';
 
 const Route = express.Router();
 
 Route.get('/login', login);
-
+Route.get('/admin', getAllAdmin);
+Route.post('/admin', createAdmin);
 
 export default Route;
